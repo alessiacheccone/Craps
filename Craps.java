97845ed76c5e054dec33a14ed564914651562401
Alessia Checcone <aecheccone@gmail.com>
2021-02-04 13:39:23 -0800

    The Die class works

--- Craps.java
+++ Craps.java
@@ -10,12 +10,6 @@ public class Craps
 {
     // instance variables - replace the example below with your own
     private int x;
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
