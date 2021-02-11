455e834fcb1f22724e4305e0d1bf4e5e46c46e81
Alessia Checcone <aecheccone@gmail.com>
2021-02-11 07:40:34 -0800

    Final version

--- Craps.java
+++ Craps.java
@@ -8,14 +8,13 @@
 import java.util.Scanner;
 public class Craps
 {
-    // instance variables - replace the example below with your own
     public static void main(String[] args)
     {
         Die d1 = new Die();
         Die d2 = new Die();
         Scanner in = new Scanner(System.in);
-        //Boolean playAgain = true;
-        System.out.println("LET'S PLAY CRAPS!");
+        System.out.println("Would you like to play Craps (Y/n)?");
+        String playAgain = in.nextLine();
         System.out.println("Do you need instructions (Y/n)?");
         String instructions = in.nextLine();
         if (instructions.substring(0,1).equalsIgnoreCase("y"))
@@ -33,67 +32,56 @@ public class Craps
         {
             System.out.println("Good luck!"); 
         }
-        System.out.println("Press <Enter> to roll the dice...");
-        String pause = in.nextLine();
-        int total = d1.roll() + d2.roll();
-        //while (playAgain = true)
-        //{
-        if (total == 7 || total == 11)
+        while (playAgain.substring(0,1).equalsIgnoreCase("y"))
         {
-            System.out.println("You rolled a " + total + "! You won! :D"); 
-        }
-        else if (total == 2 || total == 3 || total == 12)
-        {
-            System.out.println("Oh no! You rolled a " + total + ". You lost. :(");  
-        }
-        else 
-        {
-            // didn't win or lose 
-            System.out.println("You've rolled a " + total + ". This is now your point.");
-            int point = total; // roll is now set as the point
-            total = d1.roll() + d2.roll();
             System.out.println("Press <Enter> to roll the dice...");
-            String rollASecondTime = in.nextLine();
-            total = d1.roll() + d2.roll();
-            while (total != 7 && total != point)
+            String pause = in.nextLine();
+            int total = d1.roll() + d2.roll();
+            if (total == 7 || total == 11)
             {
-                // haven't won or lost yet--need to roll again
-
-                System.out.println("You've rolled a " + total + ". Roll again.");
-                String rollAThirdTime = in.nextLine();
+                System.out.println("You rolled a " + total + "! You won! :D");
+                System.out.println("Play again (Y/n)?");
+                playAgain = in.nextLine();
+            }
+            else if (total == 2 || total == 3 || total == 12)
+            {
+                System.out.println("Oh no! You rolled a " + total + ". You lost. :(");
+                System.out.println("Play again (Y/n)?");
+                playAgain = in.nextLine();
+            }
+            else 
+            {
+                // didn't win or lose 
+                System.out.println("You've rolled a " + total + ". This is now your point.");
+                int point = total; // roll is now set as the point
                 total = d1.roll() + d2.roll();
-            }
-            if (total == point)
-            {
-                System.out.println("You rolled a " + point + ". You won! :D"); 
-            }
-            else
-            {
-                System.out.println("Oops! You rolled a 7. You lost. :(");  
-            }
-            //}
+                System.out.println("Press <Enter> to roll the dice...");
+                String rollASecondTime = in.nextLine();
+                total = d1.roll() + d2.roll();
+                while (total != 7 && total != point)
+                {
+                    // haven't won or lost yet--need to roll again
+                    System.out.println("You've rolled a " + total + ". Roll again.");
+                    String rollAThirdTime = in.nextLine();
+                    total = d1.roll() + d2.roll();
+                }
+                if (total == point)
+                {
+                    System.out.println("You rolled a " + point + ". You won! :D");
+                    System.out.println("Play again (Y/n)?");
+                    playAgain = in.nextLine();
+                }
+                else
+                {
+                    System.out.println("Oops! You rolled a 7. You lost. :("); 
+                    System.out.println("Play again (Y/n)?");
+                    playAgain = in.nextLine();
+                }
 
-            //System.out.println("Play again (Y/n)?");
-            //String reset = in.nextLine();
-
-            //{
-            //playAgain = true;
-            //reset();
-            //}
-            //else
-            //{
-            //  playAgain = false;
-            //  System.out.println("Thanks for playing!");
-            // break;
-            //}
+            }
 
         }
+        System.out.println("Goodbye!");
     }
-    //public static int reset()
-    //{
-    // Die d1 = new Die();
-    // Die d2 = new Die();
-    //int total = d1.roll() + d2.roll();
-    //return total;
-    //}
 }
+
